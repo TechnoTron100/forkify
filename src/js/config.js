416d8ced8f51dd7ea852a933b5218292ea90e0db
TechnoTron100 <technologiestron@gmail.com>
2022-07-28 21:30:53 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '78ca3fe2-d4a2-472b-bf24-73fc1458c070';
+export const MODAL_CLOSE_SEC = 2.5;
